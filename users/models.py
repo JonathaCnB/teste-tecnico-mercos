ef2a7fab65c8f2504577004d34b5027c11b6a69f
JonathaCnB <jotacarlosftb@gmail.com>
2021-03-09 16:33:41 -0400

    add class abstract user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,6 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# para usar em uma futura extensão da class User
+class User(AbstractUser):
+    pass
