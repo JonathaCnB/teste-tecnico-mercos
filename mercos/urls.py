2782c654a6d0b217368a79484199e4e43a9ba294
JonathaCnB <jotacarlosftb@gmail.com>
2021-03-10 21:37:58 -0400

    add config para servir arquivos de media em desenvolvimento

--- mercos/urls.py
+++ mercos/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -5,4 +7,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     # Direcionamento para pagina principal
     path("", include("pages.urls")),
+    # Direcionamento para pagina produtos
+    path("products/", include("products.urls")),
 ]
+
+#  configuração para servir os arquivos de midia localmente
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
